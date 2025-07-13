c2df9d5c72e24b304fffc5fb4704bfb7eb63e452
ontech7 <losavio.andrea@hotmail.com>
2025-07-13 15:42:32 +0200

    Added required dependencies

---
+++ lib/db.ts
@@ -0,0 +1,19 @@
+import { neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+neonConfig.poolQueryViaFetch = true;
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+const connectionString = `${process.env.DATABASE_POOLING_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
+export const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
